416315046cce510eedd12621772b80744a806f89
Fernanda <frpalaria@gmail.com>
2023-09-15 00:24:19 -0300

    schema

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import { fastify } from 'fastify'
+import { getAllPromptsRoute } from './routes/get-all-prompts'
 
 const app = fastify()
 
-app.get('/', () => {
-    return 'Hello World'
-})
+// Todos os modulso usando o "register" tem que ser asyncrono
+app.register(getAllPromptsRoute)
 
 app.listen({
     port: 3333
